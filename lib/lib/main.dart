0e7503054f531062fcdffe5926657374965a0bcc
Platotel3s <yudhaplato@gmail.com>
2025-03-05 23:39:58 +0700

    Menamabahkan beberapa tampilan menarik

--- lib/lib/main.dart
+++ lib/lib/main.dart
@@ -1,122 +1,245 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gunting Batu Kertas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GamePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class GamePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _GamePageState createState() => _GamePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GamePageState extends State<GamePage> {
+  String _pilihanUser = '';
+  String _pilihanKomputer = '';
+  String _hasil = '';
+  int _skorUser = 0;
+  int _skorKomputer = 0;
+  bool _berhenti = false;
 
-  void _incrementCounter() {
+  void _pilihGunting() {
+    if (!_berhenti) {
+      setState(() {
+        _pilihanUser = 'Gunting';
+        _pilihanKomputer = _generatePilihanKomputer();
+        _hasil = _determineHasil();
+        _updateSkor();
+      });
+    }
+  }
+
+  void _pilihBatu() {
+    if (!_berhenti) {
+      setState(() {
+        _pilihanUser = 'Batu';
+        _pilihanKomputer = _generatePilihanKomputer();
+        _hasil = _determineHasil();
+        _updateSkor();
+      });
+    }
+  }
+
+  void _pilihKertas() {
+    if (!_berhenti) {
+      setState(() {
+        _pilihanUser = 'Kertas';
+        _pilihanKomputer = _generatePilihanKomputer();
+        _hasil = _determineHasil();
+        _updateSkor();
+      });
+    }
+  }
+
+  String _generatePilihanKomputer() {
+    List<String> pilihan = ['Gunting', 'Batu', 'Kertas'];
+    return pilihan[Random().nextInt(pilihan.length)];
+  }
+
+  String _determineHasil() {
+    if (_pilihanUser == _pilihanKomputer) {
+      return 'Seri!';
+    } else if (_pilihanUser == 'Gunting' && _pilihanKomputer == 'Kertas') {
+      return 'Anda menang!';
+    } else if (_pilihanUser == 'Batu' && _pilihanKomputer == 'Gunting') {
+      return 'Anda menang!';
+    } else if (_pilihanUser == 'Kertas' && _pilihanKomputer == 'Batu') {
+      return 'Anda menang!';
+    } else {
+      return 'Komputer menang!';
+    }
+  }
+
+  void _updateSkor() {
+    if (_hasil == 'Anda menang!') {
+      _skorUser++;
+    } else if (_hasil == 'Komputer menang!') {
+      _skorKomputer++;
+    }
+
+    // Cek apakah permainan berhenti (misalnya, jika skor mencapai 5)
+    if (_skorUser == 5 || _skorKomputer == 5) {
+      _berhenti = true;
+    }
+  }
+
+  void _resetPermainan() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _pilihanUser = '';
+      _pilihanKomputer = '';
+      _hasil = '';
+      _skorUser = 0;
+      _skorKomputer = 0;
+      _berhenti = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Gunting Batu Kertas'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _resetPermainan,
+            tooltip: 'Mulai Ulang',
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            // Skor
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Anda',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '$_skorUser',
+                      style: TextStyle(fontSize: 40, color: Colors.blue),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Komputer',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '$_skorKomputer',
+                      style: TextStyle(fontSize: 40, color: Colors.red),
+                    ),
+                  ],
+                ),
+              ],
             ),
+            SizedBox(height: 30),
+            // Pilihan
+            Text(
+              'Pilihan Anda: $_pilihanUser',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              'Pilihan Komputer: $_pilihanKomputer',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            // Hasil
+            AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              child: Text(
+                _hasil,
+                key: ValueKey(_hasil),
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: _getHasilColor(),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            // Tombol Pilihan
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildChoiceButton('Gunting', Icons.cut, _pilihGunting),
+                _buildChoiceButton('Batu', Icons.landscape, _pilihBatu),
+                _buildChoiceButton('Kertas', Icons.article, _pilihKertas),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Tombol Mulai Ulang (jika permainan berhenti)
+            if (_berhenti)
+              ElevatedButton(
+                onPressed: _resetPermainan,
+                child: Text(
+                  'Mulai Ulang',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildChoiceButton(String label, IconData icon, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: _berhenti ? null : onPressed,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, size: 40),
+          SizedBox(height: 10),
+          Text(label, style: TextStyle(fontSize: 16)),
+        ],
+      ),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  Color _getHasilColor() {
+    if (_hasil == 'Anda menang!') {
+      return Colors.green;
+    } else if (_hasil == 'Komputer menang!') {
+      return Colors.red;
+    } else {
+      return Colors.orange;
+    }
   }
 }
